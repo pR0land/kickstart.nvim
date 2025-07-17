20ff375e9387331ca9924ad77251aa5c31af2c0f
pR0land <f.roland.c@gmail.com>
2025-07-17 17:39:41 +0200

    Updated the current buildin git

--- lua/pR0land/lazy/buildin_git.lua
+++ lua/pR0land/lazy/buildin_git.lua
@@ -1,7 +1,7 @@
 return {
   'tpope/vim-fugitive',
   config = function()
-    vim.keymap.set('n', '<leader>gs', vim.cmd.Git)
+    vim.keymap.set('n', '<leader>gt', vim.cmd.Git, { desc = '[g]it [t]erminal' })
 
     local pR0land_buildin_git = vim.api.nvim_create_augroup('pR0land_buildin_git', {})
 
@@ -15,15 +15,14 @@ return {
         end
 
         local bufnr = vim.api.nvim_get_current_buf()
-        local opts = { buffer = bufnr, remap = false }
-        vim.keymap.set('n', '<leader>p', function()
+        vim.keymap.set('n', '<leader>gp', function()
           vim.cmd.Git 'push'
-        end, opts)
+        end, { buffer = bufnr, remap = false, desc = ' [g]it [p]ush}' })
 
         -- rebase always
-        vim.keymap.set('n', '<leader>P', function()
+        vim.keymap.set('n', '<leader>gP', function()
           vim.cmd.Git { 'pull', '--rebase' }
-        end, opts)
+        end, { buffer = bufnr, remap = false, desc = ' [g]it [P]ull}' })
 
         -- NOTE: It allows me to easily set the branch i am pushing and any tracking
         -- needed if i did not set the branch up correctly
